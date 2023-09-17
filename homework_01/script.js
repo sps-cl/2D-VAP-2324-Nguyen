a397f9a4cb7fdaec0b864c1b0ddf8131a5b0da47
Baojekral <baonguyenthe007@gmail.com>
2023-09-17 22:38:24 +0200

    homework_01

--- homework_01/script.js
+++ homework_01/script.js
@@ -2,7 +2,7 @@ console.log("ja jsem king bao");
 var div=document.getElementById("div");
 var button=document.getElementById("button");
 var text= document.createElement("p");
-text.textContent="smrdis"
+text.textContent="jsem king"
 button.addEventListener("click", (event)=>{
     div.appendChild(text);
 
